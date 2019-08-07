61e5164764594dd24c4ab4376bcc9520c1f6d599
yexingyun <yexingyun9210@gmail.com>
2019-08-07 11:17:08 +0700

    feat(简单描述):

--- builder/put.go
+++ builder/put.go
@@ -11,6 +11,9 @@ import (
 更新代码格式
 */
 
+//1. 收集包含特定scope的字段集合
+//2. 数据集合做下面判断
+//3. 在编码过冲中，将数据做如下判断
 func UserUpdateDTO(value map[string]interface{}, scope int) (updater interface{}, err error) {
 	if value == nil || len(value) == 0 {
 		return nil, errors.New("value nil")
--- testdata/models/user.go
+++ testdata/models/user.go
@@ -1,7 +1,12 @@
 package models
 
+// 用户表
+// 说明
 type User struct {
-	Username     string `json:"username" build:"post,put,patch"`
-	PassWord     string `json:"password" bson:"pwd"`
-	RightOfLogin bool   `json:"right_of_login" build:"post,put,patch interface"`
+	// 用户名称
+	Username string `json:"username" build:"post,put,patch"`
+	// 密码
+	PassWord string `json:"password" bson:"pwd"`
+	// 登陆权限
+	RightOfLogin bool `json:"right_of_login" build:"post,put,patch interface"`
 }
